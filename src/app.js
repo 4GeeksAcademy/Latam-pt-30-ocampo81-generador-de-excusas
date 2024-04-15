e43f8f9bc145bcb8facd4ecdd0c81c52d3f8a763
Kevin Ocampo <161964924+Ocampo81@users.noreply.github.com>
2024-04-15 20:09:47 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function generadorDeExcusa() {
+  //Arrays
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [  "before the class","when I was sleeping","while I was exercising","during my lunch","while I was praying"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//generar aleatoriamente entre los arrays
+const whoRandom = Math.floor(Math.random() * who.length);
+const actionRandom = Math.floor(Math.random() * action.length);
+const whatRandom = Math.floor(Math.random() * what.length);
+const whenRandom = Math.floor(Math.random() * when.length);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+//concatenar para acceder a la posicion de cada arreglo con el generador
+const excusa = `${who[whoRandom]} ${action[actionRandom]}${what[whatRandom]}${when[whenRandom]}.`;
+
+  return excusa;
+}
+// Función para obtener el elemento HTML con el ID "excuse"
+function mostrarExcusa() {
+  let excuseContent = document.getElementById("excuse");
+
+  // Aquí se generará la excusa
+  let excuse = generarExcusa();
+
+  excuseContent.innerHTML = excuse;
+}
+
+onload = mostrarExcusa();
