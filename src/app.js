1ae31c88e23fa2f14629df1778bd62fb24bfe2a7
Kevin Ocampo <161964924+Ocampo81@users.noreply.github.com>
2024-04-16 14:11:49 +0000

    revisión 1

--- src/app.js
+++ src/app.js
@@ -3,16 +3,22 @@ function generadorDeExcusa() {
   let who = ["The dog", "My grandma", "The mailman", "My bird"];
   let action = ["ate", "peed", "crushed", "broke"];
   let what = ["my homework", "my phone", "the car"];
-  let when = [  "before the class","when I was sleeping","while I was exercising","during my lunch","while I was praying"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
 
-//generar aleatoriamente entre los arrays
-const whoRandom = Math.floor(Math.random() * who.length);
-const actionRandom = Math.floor(Math.random() * action.length);
-const whatRandom = Math.floor(Math.random() * what.length);
-const whenRandom = Math.floor(Math.random() * when.length);
+  //generar aleatoriamente entre los arrays
+  const whoRandom = Math.floor(Math.random() * who.length);
+  const actionRandom = Math.floor(Math.random() * action.length);
+  const whatRandom = Math.floor(Math.random() * what.length);
+  const whenRandom = Math.floor(Math.random() * when.length);
 
-//concatenar para acceder a la posicion de cada arreglo con el generador
-const excusa = `${who[whoRandom]} ${action[actionRandom]}${what[whatRandom]}${when[whenRandom]}.`;
+  //concatenar para acceder a la posicion de cada arreglo con el generador
+  const excusa = `${who[whoRandom]} ${action[actionRandom]} ${what[whatRandom]} ${when[whenRandom]}.`;
 
   return excusa;
 }
@@ -21,7 +27,7 @@ function mostrarExcusa() {
   let excuseContent = document.getElementById("excuse");
 
   // Aquí se generará la excusa
-  let excuse = generarExcusa();
+  let excuse = generadorDeExcusa();
 
   excuseContent.innerHTML = excuse;
 }
